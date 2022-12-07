c26180a1e1b5bed52aed1529034a6ada33dcbfb8
github-action-benchmark <github@users.noreply.github.com>
2022-12-07 05:59:58 +0000

    add Rust Benchmark (cargo) benchmark result for 17b06555e567638dc84f6433e170d709d74e7e96

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1670392798875,
+  "repoUrl": "https://github.com/ipvm-wg/ipvm",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@brooklynzelenka.com",
+            "name": "Brooklyn Zelenka",
+            "username": "expede"
+          },
+          "committer": {
+            "email": "hello@brooklynzelenka.com",
+            "name": "Brooklyn Zelenka",
+            "username": "expede"
+          },
+          "distinct": true,
+          "id": "17b06555e567638dc84f6433e170d709d74e7e96",
+          "message": "init",
+          "timestamp": "2022-12-06T21:56:19-08:00",
+          "tree_id": "f9783d8bade9821e8b4f37148149f56519660ff0",
+          "url": "https://github.com/ipvm-wg/ipvm/commit/17b06555e567638dc84f6433e170d709d74e7e96"
+        },
+        "date": 1670392797708,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
