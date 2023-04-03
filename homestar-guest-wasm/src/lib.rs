3bafd77ccb814b8f5d06f19451b4017aeae9974a
Brian Ginsburg <gins@brianginsburg.com>
2023-04-03 14:37:41 -0700

    feat: add image processing examples
    Add blur, crop, grayscale, and rotate90 image processing functions.

--- homestar-guest-wasm/src/lib.rs
+++ homestar-guest-wasm/src/lib.rs
@@ -1,3 +1,9 @@
+use std::path::Path;
+use image::{
+    DynamicImage
+};
+
+
 wit_bindgen::generate!("test" in "./wits");
 
 struct Component;
@@ -20,6 +26,35 @@ impl Homestar for Component {
         (0..len)
             .map(|i| matrix.iter().map(|row| row[i]).collect())
             .collect()
+    }
+
+    fn blur(data: Vec<u8>, sigma: f32, width: u32, height: u32) -> Vec<u8> {
+        let img_buf= image::RgbImage::from_vec(width, height, data).unwrap();
+
+        let blurred = DynamicImage::ImageRgb8(img_buf).blur(sigma);
+        blurred.into_bytes()
+    }
+
+    fn crop(data: Vec<u8>, x: u32, y: u32, width: u32, height: u32) -> Vec<u8> {
+        let img_buf= image::RgbImage::from_vec(width, height, data).unwrap();
+
+        // Crop the bottom right rectangle
+        let cropped = DynamicImage::ImageRgb8(img_buf).crop(x, y, width, height);
+        cropped.into_bytes()
+    }
+
+    fn grayscale(data: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+        let img_buf= image::RgbImage::from_vec(width, height, data).unwrap();
+
+        let gray = DynamicImage::ImageRgb8(img_buf).grayscale();
+        gray.to_rgb8().into_vec()
+    }
+
+    fn rotate90(data: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+        let img_buf= image::RgbImage::from_vec(width, height, data).unwrap();
+
+        let rotated = DynamicImage::ImageRgb8(img_buf).rotate90();
+        rotated.into_bytes()
     }
 }
 
@@ -48,5 +83,61 @@ mod test {
         let transposed = Component::transpose(matrix.clone());
         assert_ne!(transposed, matrix);
         assert_eq!(Component::transpose(transposed), matrix);
+    }
+
+    #[test]
+    fn blur() {
+        let img = image::open(&Path::new("./fixtures/synthcat.jpg")).unwrap();
+        let (width, height) = (img.width(), img.height());
+        let img_vec = img.into_bytes();
+
+        // Call component to blur the image
+        let result = Component::blur(img_vec, 10.0, width, height);
+
+        let processed_buf= image::RgbImage::from_vec(width, height, result).unwrap();
+        let processed = DynamicImage::ImageRgb8(processed_buf);
+        processed.save("./fixtures/blurred.jpg").expect("Failed to write cropped.jpg to filesystem");
+    }
+
+    #[test]
+    fn crop() {
+        let img = image::open(&Path::new("./fixtures/synthcat.jpg")).unwrap();
+        let (width, height) = (img.width(), img.height());
+        let img_vec = img.into_bytes();
+
+        // Call component to crop the image to the bottom right corner
+        let result = Component::crop(img_vec, 200, 200, width, height);
+
+        let processed_buf= image::RgbImage::from_vec(width - 200, height - 200, result).unwrap();
+        let processed = DynamicImage::ImageRgb8(processed_buf);
+        processed.save("./fixtures/cropped.jpg").expect("Failed to write cropped.jpg to filesystem");
+    }
+
+    #[test]
+    fn grayscale() {
+        let img = image::open(&Path::new("./fixtures/synthcat.jpg")).unwrap();
+        let (width, height) = (img.width(), img.height());
+        let img_vec = img.into_bytes();
+
+        // Call component to grayscale the image
+        let result = Component::grayscale(img_vec, width, height);
+
+        let processed_buf= image::RgbImage::from_vec(width, height, result).unwrap();
+        let processed = DynamicImage::ImageRgb8(processed_buf);
+        processed.save("./fixtures/graycat.jpg").expect("Failed to write graycat.jpg to filesystem");
+    }
+
+    #[test]
+    fn rotate() {
+        let img = image::open(&Path::new("./fixtures/synthcat.jpg")).unwrap();
+        let (width, height) = (img.width(), img.height());
+        let img_vec = img.into_bytes();
+
+        // Call component to rotate the image 90 deg clockwise
+        let result = Component::rotate90(img_vec, width, height);
+
+        let processed_buf= image::RgbImage::from_vec(width, height, result).unwrap();
+        let processed = DynamicImage::ImageRgb8(processed_buf);
+        processed.save("./fixtures/rotated.jpg").expect("Failed to write graycat.jpg to filesystem");
     }
 }
