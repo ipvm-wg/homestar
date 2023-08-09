cee37d4222a048605ec6422959e69f7c7a1b713b
Euan <68219791+euamcg@users.noreply.github.com>
2023-08-09 09:42:40 +0100

    Feat: Add capture for windows signals (#234)
    # Description
    
    Adds code to capture windows signals in the runtime. Includes tests for
    starting and killing the runtime, also includes a change so github
    actions will build a windows container.
    
    ## Link to issue
    
    Fixes: [#183]
    
    ## Type of change
    - [X] New feature (non-breaking change that adds functionality)
    
    Please delete options that are not relevant.
    
    ## Test plan (required)
    Added a few tests into the CLI around testing both windows and
    non-windows functionality. Includes some ipv4 testing, daemon testing
    and windows signal testing.

--- homestar-runtime/src/runner.rs
+++ homestar-runtime/src/runner.rs
@@ -21,9 +21,12 @@ use libipld::Cid;
 #[cfg(not(test))]
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::{ops::ControlFlow, rc::Rc, sync::Arc, task::Poll};
+#[cfg(not(windows))]
+use tokio::signal::unix::{signal, SignalKind};
+#[cfg(windows)]
+use tokio::signal::windows;
 use tokio::{
     runtime, select,
-    signal::unix::{signal, SignalKind},
     sync::{mpsc, oneshot},
     task::{AbortHandle, JoinHandle},
     time,
@@ -416,6 +419,7 @@ impl Runner {
 
     /// Captures shutdown signals for [Runner].
     #[allow(dead_code)]
+    #[cfg(not(windows))]
     async fn shutdown_signal() -> Result<()> {
         let mut sigint = signal(SignalKind::interrupt())?;
         let mut sigterm = signal(SignalKind::terminate())?;
@@ -425,7 +429,22 @@ impl Runner {
             _ = sigint.recv() => info!("SIGINT received, shutting down"),
             _ = sigterm.recv() => info!("SIGTERM received, shutting down"),
         }
+        Ok(())
+    }
 
+    #[allow(dead_code)]
+    #[cfg(windows)]
+    async fn shutdown_signal() -> Result<()> {
+        let mut sigint = windows::ctrl_close()?;
+        let mut sigterm = windows::ctrl_shutdown()?;
+        let mut sighup = windows::ctrl_break()?;
+
+        select! {
+            _ = tokio::signal::ctrl_c() => info!("CTRL-C received, shutting down"),
+            _ = sigint.recv() => info!("SIGINT received, shutting down"),
+            _ = sigterm.recv() => info!("SIGTERM received, shutting down"),
+            _ = sighup.recv() => info!("SIGHUP received, shutting down")
+        }
         Ok(())
     }
 
