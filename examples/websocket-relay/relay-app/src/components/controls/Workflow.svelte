13241bb2558bf49a8756e9543844f0dcfa39b58e
Brian Ginsburg <gins@brianginsburg.com>
2023-08-28 16:26:29 -0700

    feat: Disable play when workflow active

--- examples/websocket-relay/relay-app/src/components/controls/Workflow.svelte
+++ examples/websocket-relay/relay-app/src/components/controls/Workflow.svelte
@@ -2,15 +2,22 @@
   import { createEventDispatcher } from "svelte";
 
   import type { Workflow } from "$lib/workflow";
+  import { workflowStore } from "../../stores";
   import PlayIcon from "$components/icons/Play.svelte";
   import Spinner from "$components/icons/Spinner.svelte";
 
   export let workflow: Workflow;
 
   const dispatch = createEventDispatcher();
+  let disabled = false;
 
   function run(workflowId: string) {
     dispatch("run", { workflowId });
+  }
+
+  $: {
+    const otherId = workflow.id === "one" ? "two" : "one";
+    disabled = $workflowStore[otherId].status === "working";
   }
 </script>
 
@@ -18,15 +25,21 @@
   class="flex flex-row content-center items-center px-2 py-1.5 bg-black text-white"
 >
   <span class="capitalize">Workflow {workflow.id}</span>
-  <span
-    class="ml-auto cursor-pointer"
-    on:click={() => run(workflow.id)}
-    on:keypress={() => run(workflow.id)}
-  >
-    {#if workflow.status === "waiting"}
+  {#if disabled}
+    <span class="ml-auto cursor-not-allowed">
+      <PlayIcon disabled={true} />
+    </span>
+  {:else if workflow.status === "waiting"}
+    <span
+      class="ml-auto cursor-pointer"
+      on:click={() => run(workflow.id)}
+      on:keypress={() => run(workflow.id)}
+    >
       <PlayIcon />
-    {:else if workflow.status === "working"}
+    </span>
+  {:else if workflow.status === "working"}
+    <span class="ml-auto cursor-progress">
       <Spinner />
-    {/if}
-  </span>
+    </span>
+  {/if}
 </div>
--- examples/websocket-relay/relay-app/src/components/icons/Play.svelte
+++ examples/websocket-relay/relay-app/src/components/icons/Play.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+  export let disabled = false;
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   width="16"
   height="16"
   viewBox="0 0 24 24"
   fill="none"
-  stroke="currentColor"
+  stroke={disabled ? "#999999" : "currentcolor"}
   stroke-width="2"
   stroke-linecap="round"
   stroke-linejoin="round"
