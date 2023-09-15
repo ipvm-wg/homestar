09b5e0f7839373482d884b2a6b7b99ff75d44172
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2023-09-14 21:45:40 -0400

    chore: windows fun

--- homestar-runtime/tests/cli.rs
+++ homestar-runtime/tests/cli.rs
@@ -307,7 +307,7 @@ fn test_signal_kill_serial() -> Result<()> {
     #[cfg(feature = "ipfs")]
     let _ = startup_ipfs();
 
-    Command::new(BIN.as_os_str())
+    let mut homestar_proc = Command::new(BIN.as_os_str())
         .arg("start")
         .arg("--db")
         .arg("homestar.db")
@@ -331,7 +331,17 @@ fn test_signal_kill_serial() -> Result<()> {
         .stdout(predicate::str::contains("::1"))
         .stdout(predicate::str::contains("pong"));
 
-    let _ = kill_homestar_process();
+    let _ = Command::new(BIN.as_os_str()).arg("stop").output();
+
+    if let Ok(None) = homestar_proc.try_wait() {
+        let _status_code = match homestar_proc.wait_timeout(Duration::from_secs(1)).unwrap() {
+            Some(status) => status.code(),
+            None => {
+                homestar_proc.kill().unwrap();
+                homestar_proc.wait().unwrap().code()
+            }
+        };
+    }
 
     Command::new(BIN.as_os_str()).arg("ping").assert().failure();
 
@@ -379,6 +389,18 @@ fn test_server_v4_serial() -> Result<()> {
         .success()
         .stdout(predicate::str::contains("127.0.0.1"))
         .stdout(predicate::str::contains("pong"));
+
+    let _ = Command::new(BIN.as_os_str()).arg("stop").output();
+
+    if let Ok(None) = homestar_proc.try_wait() {
+        let _status_code = match homestar_proc.wait_timeout(Duration::from_secs(1)).unwrap() {
+            Some(status) => status.code(),
+            None => {
+                homestar_proc.kill().unwrap();
+                homestar_proc.wait().unwrap().code()
+            }
+        };
+    }
 
     let _ = stop_all_bins();
 
