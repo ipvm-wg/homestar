4a1b62e63791aa13bfb28eeb5de2afe7f09afcb5
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2023-11-06 16:28:09 -0500

    fix: move to sub id as lookup key

--- homestar-runtime/tests/utils.rs
+++ homestar-runtime/tests/utils.rs
@@ -12,6 +12,7 @@ use retry::{delay::Fixed, retry};
 #[cfg(feature = "ipfs")]
 use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpStream};
 use std::{
+    future::Future,
     path::PathBuf,
     process::{Child, Command, Stdio},
     time::Duration,
@@ -19,6 +20,7 @@ use std::{
 #[cfg(not(windows))]
 use sysinfo::PidExt;
 use sysinfo::{ProcessExt, SystemExt};
+use tokio::time::{timeout, Timeout};
 use wait_timeout::ChildExt;
 
 /// Binary name, which is different than the crate name.
@@ -171,3 +173,28 @@ pub(crate) fn kill_homestar_daemon() -> Result<()> {
 
     Ok(())
 }
+
+/// Helper extension trait which allows to limit execution time for the futures.
+/// It is helpful in tests to ensure that no future will ever get stuck forever.
+pub(crate) trait TimeoutFutureExt<T>: Future<Output = T> + Sized {
+    /// Returns a reasonable value that can be used as a future timeout with a certain
+    /// degree of confidence that timeout won't be triggered by the test specifics.
+    fn default_timeout() -> Duration {
+        // If some future wasn't done in 60 seconds, it's either a poorly written test
+        // or (most likely) a bug related to some future never actually being completed.
+        const TIMEOUT_SECONDS: u64 = 60;
+        Duration::from_secs(TIMEOUT_SECONDS)
+    }
+
+    /// Adds a fixed timeout to the future.
+    fn with_default_timeout(self) -> Timeout<Self> {
+        self.with_timeout(Self::default_timeout())
+    }
+
+    /// Adds a custom timeout to the future.
+    fn with_timeout(self, timeout_value: Duration) -> Timeout<Self> {
+        timeout(timeout_value, self)
+    }
+}
+
+impl<T, U> TimeoutFutureExt<T> for U where U: Future<Output = T> + Sized {}
