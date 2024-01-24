80bfdbc932df067db9050d441705f215da472089
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2024-01-23 19:45:46 -0500

    chore: ..

--- homestar-runtime/src/main.rs
+++ homestar-runtime/src/main.rs
@@ -21,7 +21,6 @@ fn main() -> Result<()> {
             // Load settings first, so we can daemonize before starting the
             // runtime.
             let settings = if let Some(file) = runtime_config {
-                println!("Loading settings from file: {}", file.display());
                 Settings::load_from_file(file)
             } else {
                 Settings::load()
